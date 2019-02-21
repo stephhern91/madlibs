1b2fe7a2b1271963534db1c0ca9c4da50ae8ad4f
Stephanie Hernandez <stephaniehernandez@stephanies-mbp.lan>
2019-02-20 20:00:23 -0700

    Added generate and clear button

--- src/components/card.js
+++ src/components/card.js
@@ -3,38 +3,53 @@ import React, { Component } from 'react';
 import Input from './input';
 import Content from './content';
 
+const INITIAL_STATE = {
+    color: '',
+    pluralNoun: '',
+    adjectiveOne: '',
+    celebOne: '',
+
+    adjectiveTwo: '',
+    nounOne: '',
+    numberOne: '',
+    numberTwo: '',
+
+    nounTwo: '',
+    adjectiveThree: '',
+    celebTwo: '',
+    celebThree: '',
+
+    adjectiveFour: '',
+    nounThree: '',
+    celebFour: '',
+    adjectiveFive: '',
+
+    contentVisible: false
+}
+
 class Card extends Component {
 
     constructor() {
         super()
 
-        this.state = {
-            color: '',
-            pluralNoun: '',
-            adjectiveOne: '',
-            celebOne: '',
-
-            adjectiveTwo: '',
-            nounOne: '',
-            numberOne: '',
-            numberTwo: '',
-
-            nounTwo: '',
-            adjectiveThree: '',
-            celebTwo: '',
-            celebThree: '',
-
-            adjectiveFour: '',
-            nounThree: '',
-            celebFour: '',
-            adjectiveFive: '',
-        }
+        this.state = INITIAL_STATE
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
 
     handleInputChange(event) {
         this.setState({ [event.target.name]: event.target.value})
+    }
+
+    handleFormSubmit(event) {
+        event.preventDefault()
+
+        if(this.state.contentVisible) {
+            this.setState(INITIAL_STATE)
+        } else {
+            this.setState({ contentVisible: true })
+        }
     }
 
     render() {
@@ -62,10 +77,15 @@ class Card extends Component {
         ]
 
         return (
-            <div className="card">
-                { inputData.map(data => Input( (data), this.handleInputChange )) }
-                <Content data={this.state}/>
-            </div>
+            <form onSubmit={this.handleFormSubmit} className="card">
+                <div className="card__inputs">
+                    { inputData.map(data => Input( (data), this.handleInputChange )) }
+                </div>
+                <button type="submit">{this.state.contentVisible ? 'Clear Mad Lib' : 'Generate Mad Lib'}</button>
+                {
+                    this.state.contentVisible ? <Content data={this.state}/> : ''
+                }
+            </form>
         )
     }
 }
